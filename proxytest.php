ef2d36289a54ba3dedccdbaac24b6e8eb348553f
Namhyeon, Go <gnh1201@gmail.com>
2022-10-06 21:03:05 +0900

    Update proxytest.php

--- proxytest.php
+++ proxytest.php
@@ -6,32 +6,56 @@
 ini_set("default_socket_timeout", 1);  // must be. because of `feof()` works
 ini_set("max_execution_time", 0);
 
+// https://stackoverflow.com/questions/16934409/curl-as-proxy-deal-with-https-connect-method
+// https://stackoverflow.com/questions/12433958/how-to-parse-response-headers-in-php
+
+function parse_headers($str) { // Parses HTTP headers into an array
+    $headers = array();
+
+    $lines = preg_split("'\r?\n'", $str);
+
+    $first_line = array_shift($lines);
+    $headers['@method'] = explode(' ', $first_line);
+
+    foreach ($lines as $line) {
+        if (!preg_match('/^([^:]+):(.*)$/', $line, $out)) continue;
+        $headers[$out[1]] = trim($out[2]);
+    }
+
+    return $headers;
+}
+
 $data = json_decode(file_get_contents('php://input'), true);
 
 $buffer_size = $data['chunksize'];
-$out = base64_decode($data['data']);
 
-$port = intval($data['port']);
-$scheme = $data['scheme'];
+$relay_data = base64_decode($data['data']);
+$relay_headers = parse_headers($relay_data);
+$relay_port = intval($data['port']);
+$relay_scheme = $data['scheme'];
+$relay_hostname = $data['server'];
 
-$hostname = $data['server'];
-if ($scheme == "https") {
-    $hostname = sprintf("ssl://%s", $hostname);
-}
+switch ($relay_headers['@method'][0]) {
+    case "CONNECT": // {
+        echo sprintf("%s 200 Connection Established\r\n\r\n", $relay_headers['@method'][2]);
+        break;
+    // }
 
-$fp = fsockopen($hostname, $port, $errno, $errstr, 1);
+    default: // {
+        $fp = fsockopen($relay_hostname, $relay_port, $errno, $errstr, 1);
 
-if (!$fp) {
-    echo "$errstr ($errno)<br />\n";
-} else {
-    $out = base64_decode($data['data']);
-    fwrite($fp, $out);
+        if (!$fp) {
+            echo "$errstr ($errno)<br />\n";
+        } else {
+            fwrite($fp, $relay_data);
 
-    $buf = null;
-    while (!feof($fp) && $buf !== false) {
-        $buf = fgets($fp, $buffer_size);
-        echo $buf;
-    }
+            $buf = null;
+            while (!feof($fp) && $buf !== false) {
+                $buf = fgets($fp, $buffer_size);
+                echo $buf;
+            }
 
-    fclose($fp);
+            fclose($fp);
+        }
+    // }
 }
