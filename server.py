0ff6119d154ffddd0fbf36ecc25467c767279903
Namhyeon, Go <gnh1201@gmail.com>
2024-02-17 19:22:50 +0900

    Update server.py

--- server.py
+++ server.py
@@ -143,11 +143,14 @@ def proxy_server(webserver, port, scheme, method, url, conn, addr, data):
     try:
         print("[*] Started the request. %s" % (str(addr[0])))
 
-        try:
-            if scheme in [b'https', b'tls', b'ssl'] and method == b'CONNECT':
-                conn, data = proxy_connect(webserver, conn)
-        except Exception as e:
-            raise Exception("SSL negotiation failed. (%s:%s) %s" % (webserver.decode(client_encoding), str(port), str(e)))
+        while True:
+            try:
+                if scheme in [b'https', b'tls', b'ssl'] and method == b'CONNECT':
+                    conn, data = proxy_connect(webserver, conn)
+            except IOError as e:
+                print ("[*] Retrying SSL negotiation... (%s:%s) %s" % (webserver.decode(client_encoding), str(port), str(e)))
+            except Exception as e:
+                raise Exception("SSL negotiation failed. (%s:%s) %s" % (webserver.decode(client_encoding), str(port), str(e)))
 
         response = b''
 
