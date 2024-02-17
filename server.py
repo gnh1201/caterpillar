899b027e13b4e6d65023820f2d3e25cd8360fced
Namhyeon, Go <gnh1201@gmail.com>
2024-02-18 04:30:10 +0900

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 # Namyheon Go (Catswords Research) <gnh1201@gmail.com>
 # https://github.com/gnh1201
 # Created at: 2022-10-06
-# Updated at: 2024-12-17
+# Updated at: 2024-12-18
 
 import argparse
 import socket
