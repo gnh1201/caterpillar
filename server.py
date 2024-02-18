fc53acacc49cdcf62ab1a0884adeedd3dd3d25b7
Namhyeon, Go <gnh1201@gmail.com>
2024-02-19 08:10:20 +0900

    Update server.py

--- server.py
+++ server.py
@@ -168,8 +168,8 @@ def proxy_check_filtered(data, webserver, port, scheme, method, url):
     # ID validation with K-Anonymity
     pattern = r'[/@]([a-zA-Z0-9]{10})(?![a-zA-Z0-9])'
     matches = list(set(re.findall(pattern, text)))
-    print ("[*] Found: %s" % (', '.join(matches)))
     if len(matches) > 0:
+        print ("[*] Found ID: %s" % (', '.join(matches)))
         filtered = not all(map(validate_string_with_k_anonymity, matches))
 
     # take action
