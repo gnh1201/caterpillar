a9c0ab559f1d13772ad89dd1f444e1fe016b4593
Namhyeon, Go <gnh1201@gmail.com>
2024-03-06 15:50:01 +0900

    Update container.py

--- plugins/container.py
+++ plugins/container.py
@@ -4,7 +4,7 @@ class Container(Extension):
     def __init__(self):
         self.type = "rpcmethod"
         self.method = "container_init"
-        self.methods = ["container_run", "container_stop"]
+        self.exported_methods = ["container_run", "container_stop"]
 
     def dispatch(self, type, id, params, conn):
         pass
