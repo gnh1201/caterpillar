486a399b88a6a21b69e6f4e9f08e6805be334e74
Namhyeon, Go <gnh1201@gmail.com>
2024-03-13 17:55:55 +0900

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@
 # Namyheon Go (Catswords Research) <gnh1201@gmail.com>
 # https://github.com/gnh1201/caterpillar
 # Created at: 2022-10-06
-# Updated at: 2024-03-06
+# Updated at: 2024-03-13
 #
 
 import argparse
