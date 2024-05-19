fd0b1575c20429e7a39cce112cad990ee1460d88
Namhyeon, Go <gnh1201@gmail.com>
2024-05-20 02:19:54 +0900

    Update base.py

--- base.py
+++ base.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python3
+#
+# base.py
+# base file
+#
+# Caterpillar Proxy - The simple and parasitic web proxy SPAM spam filter
+# Namyheon Go (Catswords Research) <gnh1201@gmail.com>
+# https://github.com/gnh1201/caterpillar
+# Created at: 2024-05-20
+# Updated at: 2024-05-20
+#
+
 import hashlib
 import json
 
