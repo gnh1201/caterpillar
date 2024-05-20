ed75144e82f509a7e80627e55bbed5a47b79a8cb
Namhyeon, Go <gnh1201@gmail.com>
2024-05-21 00:04:39 +0900

    Update base.py

--- base.py
+++ base.py
@@ -7,11 +7,12 @@
 # Namyheon Go (Catswords Research) <gnh1201@gmail.com>
 # https://github.com/gnh1201/caterpillar
 # Created at: 2024-05-20
-# Updated at: 2024-05-20
+# Updated at: 2024-05-21
 #
 
 import hashlib
 import json
+import re
 
 client_encoding = 'utf-8'
 
