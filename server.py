963f14a71c8c0b766ccb270a2ca9945a0ef26a32
Namhyeon, Go <gnh1201@gmail.com>
2024-06-20 17:20:57 +0900

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 # server.py
 # server file with TCP connection mode
 #
-# Caterpillar Proxy - The simple and parasitic web proxy SPAM spam filter
+# Caterpillar Proxy - The simple web debugging proxy (formerly, php-httpproxy)
 # Namyheon Go (Catswords Research) <gnh1201@gmail.com>
 # https://github.com/gnh1201/caterpillar
 # Created at: 2022-10-06
