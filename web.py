a9783c6081baa62a635410525249dd079a0bc5aa
Namhyeon, Go <gnh1201@gmail.com>
2024-07-10 09:28:06 +0900

    One more fix #27

--- web.py
+++ web.py
@@ -7,7 +7,7 @@
 # Namyheon Go (Catswords Research) <gnh1201@gmail.com>
 # https://github.com/gnh1201/caterpillar
 # Created at: 2024-05-20
-# Updated at: 2024-07-09
+# Updated at: 2024-07-10
 #
 
 from flask import Flask, request, redirect, url_for, render_template
@@ -94,6 +94,7 @@ if __name__ == "__main__":
     Extension.set_protocol('http')
 
     # load extensions
-    map(Extension.register, use_extensions.split(','))
+    for s in use_extensions.split(','):
+        Extension.register(s)
 
     app.run(debug=True, host='0.0.0.0', port=listening_port)
